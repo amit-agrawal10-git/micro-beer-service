6f8baeb505f97f23040de2e9c545546524856f63
amit-agrawal10 <amit.agrawal10@gmail.com>
2020-09-13 00:28:39 +0530

    JMS support start

---
+++ src/main/java/guru/springframework/msscbeerservice/services/BreweryService.java
@@ -0,0 +1,44 @@
+package guru.springframework.msscbeerservice.services;
+
+import guru.springframework.msscbeerservice.config.JMSConfig;
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.events.BrewBeerEvent;
+import guru.springframework.msscbeerservice.repositories.BeerRepository;
+import guru.springframework.msscbeerservice.services.inventory.BeerInventoryService;
+import guru.springframework.msscbeerservice.web.mappers.BeerMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class BreweryService {
+    private final BeerRepository beerRepository;
+    private final BeerInventoryService beerInventoryService;
+    private final JmsTemplate jmsTemplate;
+    private final BeerMapper beerMapper;
+
+    @Scheduled(fixedRate = 5000)
+    public void checkForLowInventory(){
+        List<Beer> beers = beerRepository.findAll();
+
+        beers.forEach(
+                beer -> {
+                    Integer invQoH = beerInventoryService.getOnhandInventory(beer.getId());
+                    log.debug("Min onhand is: "+beer.getMinOnHand());
+                    log.debug("Inventory on hand is: "+invQoH);
+
+                    if(invQoH<=beer
+                    .getMinOnHand()){
+                        jmsTemplate.convertAndSend(JMSConfig.BREWING_REQUEST_QUEUE,new BrewBeerEvent(beerMapper.beerToBeerDto(beer)));
+                    }
+                }
+        );
+    }
+
+}
